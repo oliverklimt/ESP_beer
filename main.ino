bc275b348bfd81c6a12d9d1f11619bd576335a0b
Oliver Klimt <oliver.klimt@blep.cz>
2020-07-10 00:23:42 +0200

    finalized temp reading

--- main.ino
+++ main.ino
@@ -1,11 +1,17 @@
-const int numReadings = 10;
+const int numReadings = 60;
 
-int readings[numReadings];      // the readings from the analog input
+const int rezistor = 22000;
+const int termNom = 22000;
+const int refTemp = 25;
+const int beta = 3950;
+
+
+float readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
+float total = 0;                  // the running total
+float average = 0;                // the average
 
-int inputPin = A0;
+const int inputPin = A0;
 
 
 
@@ -32,8 +38,22 @@ void loop() {
 	}
 
 	// calculate the average:
-	average = total / numReadings;
+	average = total / numReadings; // THIS!!!
 	// send it to the computer as ASCII digits
 	
+	average = 4095 - average - 1;
+	average = rezistor / average;
+
+	float temp;
+	temp = average / termNom;
+	temp = log(temp);
+	temp /= beta;
+	temp += 1.0 / (refTemp+273.15);
+	temp = 1.0 / temp;
+	temp -= 273.15; // NOW there is the FINAL TEMP
+
+
+
+
 	delay(1);        // delay in between reads for stability
 }
